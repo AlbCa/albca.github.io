069761a57db854f1b17e70af3c87b6679ae095e6
AlbCa <alberto.carrera@phd.unipd.it>
2025-01-28 17:40:59 +0100

    updates

--- pelicanconf.py
+++ pelicanconf.py
@@ -41,9 +41,7 @@ SOCIAL = (('You can add links in your config file', '#'),
 STRAVA = 'https://www.strava.com/athletes/50890324'
 GITHUB = 'https://github.com/AlbCa/'
 INSTAGRAM = 'https://www.instagram.com/_herbee/'
-TWITTER = '/404.html'
 TELEGRAM = '/404.html'
-SOUNDCLOUD = 'https://soundcloud.com/user-566745693-553834459/aert-1621'
 
 # PLUGINS
 PLUGIN_PATHS = ['plugins']
